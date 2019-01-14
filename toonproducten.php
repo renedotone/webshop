a4e7271483f3bdc261706309d305b2188128e5b5
renedotone <rmspijker@gmail.com>
2019-01-14 15:00:21 +0100

    add search

--- toonproducten.php
+++ toonproducten.php
@@ -1,10 +1,25 @@
+
+<form>
+    <input name="naamfilter" placeholder="Naam bevat...">
+    <input type="submit" value="Filter">
+</form>
+
 <?php
+
+if(isset($_GET['naamfilter']))
+    {
+        $naamfilter = $_GET['naamfilter'];
+    }
+    else
+    {  
+        $naamfilter = "";
+    }    
 
 try{
     $conn = new PDO("mysql:host=localhost;dbname=webshopdb", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->query("SELECT * FROM producten");
+    $stmt = $conn->query("select * from producten WHERE naam LIKE '%$naamfilter%'");
     while ($row = $stmt->fetch()) {
         echo "<LI>" . $row['naam'] . " : " . $row['prijs'] ." ";
         echo "<a href='dbproductverwijderen.php?productid=" . $row['id'] . "'>Verwijder</a>" . " ";
